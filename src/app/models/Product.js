a32a14cd4928395aacf52894e4e178bfc27ee402
CaioMiranda12 <caioclm04@gmail.com>
2024-04-11 13:17:25 -0300

    update image way

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://localhost:3001/product-file/${this.path}`
+            return `https://api-code-burger-production-c72d.up.railway.app/product-file/${this.path}`
           },
         },
       },
